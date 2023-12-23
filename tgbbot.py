a8c0359104ba99ea6d59484c552a914110ef4ea4
tankuz <150084312+tankuz@users.noreply.github.com>
2023-12-23 12:47:50 +0500

    Update tgbbot.py

--- tgbbot.py
+++ tgbbot.py
@@ -6,7 +6,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
+app = ApplicationBuilder().token("6772726500:AAEeGP9QvL6wmYDKgKSXmr6M90-dVJRqwUs").build()
 
 app.add_handler(CommandHandler("hello", hello))
 
